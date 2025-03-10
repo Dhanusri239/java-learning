7aa0195b0482b3f548d0fcdcbc15884d4c03a7a3
Dhanusri239 <sdhanu946@gmail.com>
2025-03-10 21:54:47 +0530

    program3.java file is added

---
+++ program3.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+class program3{
+    public static void main(String args[])
+
+    {
+Scanner sc=new Scanner(System.in);
+System.out.print("enter num1:");
+int a=sc.nextInt();
+System.out.print("enter num2:");
+int b=sc.nextInt();
+System.out.print("enter num3:");
+int c=sc.nextInt();
+int d=a*b*c;
+int e=a+b+c;
+System.out.println("The result is:"+d/e);
+
+    }
+}
