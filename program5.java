124c4be01ad473fd2559f4465ba17f392e85acdf
Dhanusri239 <sdhanu946@gmail.com>
2025-03-11 18:17:43 +0530

    program5.java file is added

---
+++ program5.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class program5{
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        for(int a=n;a>=1;a=a-1){
+            System.out.println("Number"+a);
+        }
+    }
+}
