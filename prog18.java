e139775ab5ae0daee051bfc9dbffcc12f697ff2d
Dhanusri239 <sdhanu946@gmail.com>
2025-03-16 16:31:35 +0530

    prog18.java file is added

--- prog18.java
+++ prog18.java
@@ -1,16 +1,35 @@
 import java.util.Scanner;
-class prog17{
-    public static void main(String args[])
-    {
+class prog18{
+    public static void main(String args[]){
         Scanner sc=new Scanner(System.in);
-        System.out.print("Enter the value:");
-        int num1=sc.nextInt();
-        System.out.print("Enter the value:");
-        int num2=sc.nextInt();
-        String result=num1>num2?"num1 is greater":"num2 is greater";
-        System.out.println(result);
+        System.out.print("Enter the value between 1-7:");
+        int day=sc.nextInt();
+        switch(day){
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wednesday");
+                break;
+            case 4:
+                System.out.println("Thursday"); 
+                break;
+            case 5:
+                System.out.println("Friday"); 
+                break;  
+            case 6:
+                System.out.println("saturday");
+                break;
+            case 7:
+                System.out.println("Sunday");
+                break;
+            default:
+                System.out.println("Invalid value");
 
-
+        }
 
     }
 }
