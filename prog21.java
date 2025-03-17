a87d328fc22304d3a3ea87ee096da9f97e05b518
Dhanusri239 <sdhanu946@gmail.com>
2025-03-17 21:32:00 +0530

    prog22.java file is added

--- prog21.java
+++ prog21.java
@@ -25,11 +25,10 @@ class prog21{
         for(int i=1;i<=100;i=i+1){
             if(i%3==0 && i%5==0){
                 System.out.println("The number divisible by 3&5 are:"+i);
-            }
-        }
+            }  
         
         
         }
-
+    }
 
 }
---
+++ prog22.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+class prog22{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int [] num=new int[5];
+        num[0]=sc.nextInt();
+        num[1]=sc.nextInt();
+        num[2]=sc.nextInt();
+        num[3]=sc.nextInt();
+        num[4]=sc.nextInt();
+        System.out.println(num[0]+num[1]+num[2]
+        +num[3]+num[4]);
+   
+   
+    }
+
+        
+}
