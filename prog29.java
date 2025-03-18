bf55f3922d139ebf37753bdc39017f36bd5b235c
Dhanusri239 <sdhanu946@gmail.com>
2025-03-18 22:32:52 +0530

    prog29.java file is added

---
+++ prog29.java
@@ -0,0 +1,18 @@
+class prog29{
+    public static void main(String args[]){
+        //Type conversion
+        byte b=20;
+        int a=b;
+        System.out.println("The type conversion is" + a);
+    
+    //Type casting
+    double d=2.22;
+    int n= (int)d;
+    System.out.println("The type casting is" + n);
+    //Type promotion
+    byte e=10;
+    byte f=20;
+
+     System.out.println("The type promotion is"+ (e*f));
+    }
+}
