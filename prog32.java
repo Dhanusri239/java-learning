47da30f5af09784acd6d6d8f6d3b5136180e0511
Dhanusri239 <sdhanu946@gmail.com>
2025-03-19 22:45:29 +0530

    prog32.java file is added

---
+++ prog32.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+class prog32{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int num=sc.nextInt();
+        boolean ifprime=true;
+        if(num<=1){
+            ifprime=false;
+        }
+        else{
+            for(int i=2;i<=Math.sqrt(num);i++){
+                if(num%2==0){
+                    ifprime=false;
+                    break;
+                }
+            }
+        }
+        System.out.println(num+ (ifprime? " is a prime number":" is not a prime number"));
+
+    }
+}
