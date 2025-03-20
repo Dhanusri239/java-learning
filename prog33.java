fe82d87d22d1ec6b46fd4dd4427f0a5035c57f64
Dhanusri239 <sdhanu946@gmail.com>
2025-03-20 23:11:45 +0530

    prog33 file is added

---
+++ prog33.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+class prog33{
+    public static void main(String args[]){
+        int n=9;
+        double d=n;
+        System.out.print("Integer value is:");
+        System.out.println(n);
+        System.out.print("Double value is:");
+        System.out.println(d);
+        //type casting
+        double changed=(double)n;
+        System.out.print("The variable that i have changed:");
+        System.out.print(changed);
+
+
+    }
+}
