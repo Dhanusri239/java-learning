8036cefa9d8a5f5ae6c0fcfbc7d3b67864de9192
Dhanusri239 <sdhanu946@gmail.com>
2025-04-09 19:14:14 +0530

    added

--- prog61.java
+++ prog61.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-class prog60{
+class prog61{
     public static void main(String[] args) {
     Scanner sc=new Scanner(System.in);
     System.out.print("Enter the number:");
