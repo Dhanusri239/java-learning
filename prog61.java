c7b7eb1fe496692eda0815ead948e5332d1d7801
Dhanusri239 <sdhanu946@gmail.com>
2025-04-09 16:27:42 +0530

    added

---
+++ prog61.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+class prog60{
+    public static void main(String[] args) {
+    Scanner sc=new Scanner(System.in);
+    System.out.print("Enter the number:");
+    int num=sc.nextInt();
+    int temp=num;
+     System.out.println("The factors of given numbers are:");
+     int sum=0;
+    for(int i=1;i<num;i++){
+        if(num%i==0){
+            System.out.print(i+" ");
+            sum=sum+i;
+        }
+    }
+      System.out.println();
+       if(temp==sum){
+          System.out.println("Its a perfect number");
+    }
+    else{
+        System.out.println("Not a Perfect number");
+    }
+}
+}
