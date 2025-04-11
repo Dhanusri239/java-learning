b3ee6ecd46c917402fe5ce83ab00639acf474fcf
Dhanusri239 <sdhanu946@gmail.com>
2025-04-11 15:10:31 +0530

    added

---
+++ prog68.java
@@ -0,0 +1,35 @@
+import java.util.*;
+public class prog68 {
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        int arr[][]=new int[a][b];
+        for(int i=0;i<a;i++){
+            for(int j=0;j<b;j++)
+            {
+             arr[i][j]=sc.nextInt();
+            }         
+           }  
+           int temp[]=new int[b];
+           for(int i=0;i<a;i++){
+            temp[i]=arr[i][0];
+        }
+        for(int i=0;i<a;i++){
+            for(int j=0;j<b-1;j++){
+                arr[i][j]=arr[i][j+1];
+            }
+        }
+        for(int i=0;i<a;i++){
+            arr[i][b-1]=temp[i];
+        }
+
+           for(int i=0;i<a;i++){
+            for(int j=0;j<b;j++)
+            {
+              System.out.print(arr[i][j]+" ");
+           }  
+           System.out.println();       
+           }  
+    }
+}
