dc2c5926e012caf832e4a27d8f0774a3dc289bf4
Dhanusri239 <sdhanu946@gmail.com>
2025-04-29 14:35:24 +0530

    add

---
+++ prog114.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class prog114 {
+    public static class Engine{
+        void start(){
+            System.out.println("Engine started..");
+        }
+    }
+    public static void main(String args[]){
+        prog114.Engine o=new prog114.Engine();
+        o.start();
+    }
+}
